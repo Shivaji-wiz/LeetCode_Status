37c793d1954049e3249cfd2fa7cc37c465f09ffc
Shivaji-wiz <85451155+Shivaji-wiz@users.noreply.github.com>
2022-02-22 01:18:20 +0530

    Time: 201 ms (10.97%), Space: 37.9 MB (69.66%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
@@ -1,5 +1,16 @@
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        return nums[-k]
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+        for(int i=0;i<k;i++)//iterating array k  times to get kth element
+            for(int  j=i;j<nums.Length;j++)
+            {
+                if(nums[i]<nums[j])
+                {
+                    int t=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=t;
+                }
+            }
+        return nums[k-1];        
         
+    }
+}
