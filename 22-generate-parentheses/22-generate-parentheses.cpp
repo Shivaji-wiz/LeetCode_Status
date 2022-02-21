62013781bc58f8236468a8e28d5984fbdf622645
Shivaji-wiz <85451155+Shivaji-wiz@users.noreply.github.com>
2022-02-22 00:57:27 +0530

    Time: 4 ms (73.77%), Space: 11.5 MB (81.41%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    vector<string>val;
+    
+    void gen(string &s,int open,int close){
+        if(open==0 && close==0){
+            val.push_back(s);
+            return;
+        }
+        if(open>0){
+            s.push_back('(');
+            gen(s,open-1,close);
+            s.pop_back();
+        }
+        
+        if(close>0){
+            if(open<close){
+                s.push_back(')');
+                gen(s,open,close-1);
+                s.pop_back();
+            }
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        string s;
+        gen(s,n,n);
+        return val;
+        
+    }
+};
