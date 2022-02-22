8a200e746dc6626365567d80cc3ee56f49fe2f27
Shivaji-wiz <85451155+Shivaji-wiz@users.noreply.github.com>
2022-02-22 15:53:21 +0530

    Time: 172 ms (22.99%), Space: 54.8 MB (11.68%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        set<int>s;
+        
+        for(auto &x:nums)
+        s.insert(x);
+        
+        return (nums.size()!=s.size());
+        
+    }
+};
