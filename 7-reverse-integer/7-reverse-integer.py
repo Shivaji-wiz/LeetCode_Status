b72a37ffd7835592f5bfc71af4c18d559b5f7e75
Shivaji-wiz <85451155+Shivaji-wiz@users.noreply.github.com>
2022-02-22 22:23:05 +0530

    Time: 41 ms (59.56%), Space: 13.9 MB (65.29%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        rev = int(str(abs(x))[::-1])
+        if rev>=2**31 - 1:
+            return 0
+        
+        if x<0:
+            return rev * -1
+        else:
+            return rev
+        
