df587781958274183532dbb7a810251496c04acd
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-02-26 23:09:01 +0530

    Time: 35 ms (47.16%), Space: 20.8 MB (16.84%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int r = board.size();
+        int c = board[0].size();
+        
+        unordered_map<char,int>rows[9];
+        unordered_map<char,int>cols[9];
+        unordered_map<char,int>boxes[9];
+        
+        for(int i = 0;i<r;i++){
+            for(int j = 0;j<c;j++){
+                char ch = board[i][j];
+                
+                if(ch!='.' && (rows[i][ch]++ > 0 || cols[j][ch]++ > 0 || boxes[i/3*3+j/3][ch]++ > 0))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
