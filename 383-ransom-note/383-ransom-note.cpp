d4997748802c5eb4cd05c8bf15ea0feb9a0315d6
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-02-27 11:48:52 +0530

    Time: 40 ms (17.42%), Space: 8.7 MB (91.56%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,12 +1,13 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        data = list(magazine)
-        flag = True
-        for i in ransomNote:
-            if i in data:
-                data.remove(i)
-            else:
-                flag = False
-                break
-        return flag
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> umap;
         
+        for(int i = 0; ransomNote[i]; i++) umap[ransomNote[i]]++;
+        for(int i = 0; magazine[i]; i++) umap[magazine[i]]--;
+        for(int i = 0; ransomNote[i]; i++){
+            if(umap[ransomNote[i]] > 0) return false;
+        }
+        return true;   
+    }
+};
