8735b1d9921657bc00570183831d2b588b916d79
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-03-06 15:33:17 +0530

    Time: 3 ms (34.47%), Space: 6.1 MB (83.92%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
-    string interpret(string command) {
-        command = regex_replace(command, regex("\\(al\\)"), "al");
-        command = regex_replace(command, regex("\\(\\)"), "o");
-        return command;
-        
+    string interpret(string s) {
+        string ans;
+        for (int i = 0; i < s.size(); ) {
+            if (s[i] == 'G') ans += 'G', ++i;
+            else if (s[i + 1] == ')') ans += 'o', i += 2;
+            else ans += "al", i += 4;
+        }
+        return ans;
     }
 };
