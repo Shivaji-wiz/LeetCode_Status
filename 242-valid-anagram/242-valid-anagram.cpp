9cb40df79e8f9463c85e6111ea3239caf76be27c
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-03-09 15:50:29 +0530

    Time: 16 ms (42.28%), Space: 7.4 MB (46.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        if(s.size()!=t.size())return false;
-        unordered_map<char,int>mp;
-        for(auto i:s)mp[i]++;
-        for(auto i:t)mp[i]--;
-        
-        for(auto i:mp) if(i.second!=0)return false;
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+	
+    if(s == t){
         return true;
+    }
+    return false;        
     }
 };
