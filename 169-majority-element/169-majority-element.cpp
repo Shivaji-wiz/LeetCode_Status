20376f199affff0bb2e1959e1f3115eea8ad88e9
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-03-28 08:40:29 +0530

    Time: 45 ms (7.04%), Space: 19.6 MB (83.84%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int m_e = nums[0];
         int count = 1;
-        for(int i = 1;i<n;i++){
+        for(int i = 0;i<n;i++){
             if(nums[i]==m_e){
                 count++;
             }
