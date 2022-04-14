70eb481dd902452cc19c4528e06a8030cf706961
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-04-14 14:21:43 +0530

    Time: 3 ms (73.10%), Space: 6.4 MB (45.12%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        int index=0;
-        for(int i=1;i<=n;++i){
-        index = (index+k)%i;
-    }
-        return index+1;        
+        vector<int> v;
+        int p=n;
+        for(int i=1; i<=n; i++)
+            v.push_back(i);
+        k--;
+        int j=0;
+        for(int i=0; i<n-1; i++)
+        {
+            v.erase(v.begin()+((k+j)%p));
+            j=(k+j)%p;   
+            p--; 
+        }
+        return v[0];        
     }
 };
