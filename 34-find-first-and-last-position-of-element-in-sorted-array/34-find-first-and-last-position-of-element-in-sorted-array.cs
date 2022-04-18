47fd62dfbedbf926f6ec74c72e6aac9ad391e8db
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-04-18 20:08:12 +0530

    Time: 153 ms (78.39%), Space: 44.7 MB (24.56%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cs
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cs
@@ -1,8 +1,15 @@
 public class Solution {
     public int[] SearchRange(int[] nums, int target) {
-        int[] index = new int[2]{-1,-1};
-       index[0] = Array.IndexOf(nums,target);
-        index[1] = Array.LastIndexOf(nums,target);      
-        return index;        
+        
+        var ind = new int[2]{-1,-1};
+        
+        ind[0] = Array.IndexOf(nums,target);
+        
+        ind[1] = Array.LastIndexOf(nums,target);
+        
+        return ind;
+                          
     }
 }
+
+
