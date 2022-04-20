0cc8d5a6d58d05dd953506b4b9ef2821d39c3cb8
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-04-20 16:14:05 +0530

    Time: 16 ms (7.29%), Space: 10.2 MB (72.18%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,3 +1,26 @@
-class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        return min(nums)
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int min = INT_MAX;
+        
+        int low = 0;
+        int high = nums.size()-1;
+        
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            
+            if(nums[mid] < min){
+                min = nums[mid];
+            }
+            
+            if(nums[mid] < nums[high]){
+                high = mid;
+            }
+            
+            else{
+                low = mid+1;
+            }
+        }
+        return min;
+    }
+};
