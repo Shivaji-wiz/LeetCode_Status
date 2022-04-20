437ca98d6495a47875d9b0accb4850194c20841d
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-04-20 16:40:30 +0530

    Time: 96 ms (73.15%), Space: 19.9 MB (87.04%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,39 +1,56 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>tri;
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        if(n<3){
-            return {};
-        }
-        if(nums[0]>0){
-            return {};
-        }
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        vector<vector<int>>triplets;
         
-        for(int i = 0;i<n;i++){
-            if(nums[i]>0)break;
-            int j = i+1,k = n-1,sum = 0;
-            if(i>0 && nums[i]==nums[i-1])continue;
+        int n=arr.size();
+        
+        sort(arr.begin(),arr.end());
+        
+        if(n<3){return {};}
+        
+        if(arr[0]>0){return {};}
+        
+        for(int i=0;i<n;i++){
+        
+            if(arr[i]>0){break;} 
+           
+            int j=i+1,k=n-1,sum=0; 
+           
+            if(i>0&&arr[i]==arr[i-1])continue;
+           
             while(j<k){
-                sum = nums[i]+nums[j]+nums[k];
+            
+                sum=arr[i]+arr[j]+arr[k];
+               
+                if(sum<0){
                 
+                    j++;
+               
+                }
+               
+                else if(sum>0){
+                
+                    k--;
+               
+                }
+               
+                else{
+                
+                    triplets.push_back({arr[i],arr[j],arr[k]});
+                   
+                    int lo_low=arr[j],lo_high=arr[k];
+                   
+                    while(j<k&&lo_low==arr[j])j++;
+                   
+                    while(j<k&&lo_high==arr[k])k--;
+               }
+                
+           }
             
-            if (sum<0){
-                j++;
-            }
-            else if(sum>0){
-                k--;
-            }
-            
-            else{
-                tri.push_back({nums[i],nums[j],nums[k]});
-                int lo = nums[j],hi = nums[k];
-                while(j<k && lo == nums[j])j++;
-                while(j<k && hi == nums[k])k--;
-            }
-        }
-        }
-        return tri;
+       }
+               
+        return triplets;
     }
+    
 };
