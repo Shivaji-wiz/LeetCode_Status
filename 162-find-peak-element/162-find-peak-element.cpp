57813573f7ee53119a7e60dfd62de6454d96f5d6
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-04-20 16:28:43 +0530

    Time: 8 ms (28.44%), Space: 8.9 MB (5.16%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,4 +1,21 @@
-class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        return nums.index(max(nums))
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size()-1;
+        int mid;
         
+        while(low<high){
+            mid = (low+high) >> 1;
+            
+            if(nums[mid] <= nums[mid+1]){
+                low = mid+1;
+            }
+            
+            else{
+                high = mid;
+            }
+        }
+        return low;
+    }
+};
