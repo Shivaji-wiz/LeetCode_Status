64f20bb97c06f0498ab424fb253d785e9ab45c7f
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-04-21 14:05:29 +0530

    Time: 175 ms (87.41%), Space: 18.9 MB (59.07%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.py
@@ -0,0 +1,27 @@
+class MyHashSet:
+
+
+    def __init__(self):
+        self.l={}
+
+    def add(self, key: int) -> None:
+        
+        self.l[key] = 1
+
+    def remove(self, key: int) -> None:
+        try:
+            self.l.pop(key)
+        except:
+            pass
+
+    def contains(self, key: int) -> bool:
+        if key in self.l:
+            return True
+        return False
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
