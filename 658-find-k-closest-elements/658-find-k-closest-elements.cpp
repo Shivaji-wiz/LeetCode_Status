a989aa136688e08c0a3ffa85e94b1859954f1e2a
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-05-04 18:12:35 +0530

    Time: 57 ms (63.94%), Space: 31.7 MB (67.37%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,7 +1,38 @@
-class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        if k == len(arr):
-            return arr
-        
-        arr = sorted(arr,key = lambda num : abs(num-x))
-        return sorted(arr[:k])
+class Solution
+{
+public:
+    vector<int> findClosestElements(vector<int> &arr, int k, int x)
+    {
+        int j = 0;
+        while(j < arr.size() and arr[j] < x) j++;
+        int i = j - 1;
+        vector<int> ans;
+
+        while ((i >= 0 or j < arr.size()) and ans.size() < k)
+        {
+            if(i < 0)
+            {
+                ans.push_back(arr[j]);
+                j++;
+                continue;
+            }
+            if(j >= arr.size()){
+                ans.push_back(arr[i]);
+                i--;
+                continue;
+            }
+            if (abs(x - arr[i]) > abs(x - arr[j]))
+            {
+                ans.push_back(arr[j]);
+                j++;
+            }
+            else
+            {
+                ans.push_back(arr[i]);
+                i--;
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
