08ac0c7d8de8e5ba3ecea598f86f951c576b9b58
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-06-04 21:20:59 +0530

    Time: 3 ms (96.12%), Space: 7.2 MB (67.79%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
     
-    bool isSafe(int row,int col,vector<string> board,int n){
-        int dr = row;
-        int dc = col;
-        
-        while(row>=0 and col>=0){
-            if(board[row][col]=='Q')return false;
-            row--;
-            col--;
-        }
-        
-        col = dc;
-        row = dr;
-        while(col>=0){
-            if(board[row][col]=='Q')return false;
-            col--;
-        }
-        
-        row = dr;
-        col = dc;
-        while(row<n and col>=0){
-            if(board[row][col]=='Q')return false;
-            row++;
-            col--;
-        }
-        return true;
-    }
-    
-    void solve(int col,vector<string> &board,vector<vector<string>> &ans,int n){
+    void solve(int col,vector<string> &board,vector<vector<string>> &ans,vector<int> &leftRow,vector<int> &upperDiagonal,vector<int> &lowerDiagonal,int n){
         if(col==n){
             ans.push_back(board);
             return;
         }
         
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,board,n)){
+            if(leftRow[row]==0 and lowerDiagonal[row+col]==0 and upperDiagonal[n-1+col-row]==0){
                 board[row][col] = 'Q';
-                solve(col+1,board,ans,n);
+                leftRow[row]=1;
+                lowerDiagonal[row+col]=1;
+                upperDiagonal[n-1+col-row]=1;
+                
+                solve(col+1,board,ans,leftRow,upperDiagonal,lowerDiagonal,n);
                 board[row][col] = '.';
+                leftRow[row]=0;
+                lowerDiagonal[row+col]=0;
+                upperDiagonal[n-1+col-row]=0;
             }
         }
     }
@@ -52,7 +32,9 @@ public:
             board[i] = s;
         }
         
-        solve(0,board,ans,n);
+        vector<int> leftRow(n,0), upperDiagonal(2*n-1,0),lowerDiagonal(2*n-1,0);
+        
+        solve(0,board,ans,leftRow,upperDiagonal,lowerDiagonal,n);
         return ans;
     }
 };
