a63e1559b7639239e9203589f08143721375afd8
Shivaji Saxena <shivaji1710saxena@gmail.com>
2022-07-11 12:21:47 +0530

    Time: 4 ms (100.00%), Space: 11.8 MB (100.00%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
-    int fillCups(vector<int>& amount) {
-
-        return max(*max_element(begin(amount),end(amount)),(accumulate(begin(amount),end(amount),0)+1)/2);
+    int fillCups(vector<int>& a) {
+        
+        sort(a.begin(),a.end());
+        
+        int sum = 0;
+        
+        int cold = a[0];
+        int warm = a[1];
+        int hot = a[2];
+        
+        sum = cold + warm + hot;
+        
+        if(cold + warm > hot) return sum/2+sum%2;
+        
+        if(cold == 0 && warm == 0)return hot;
+        return hot;
     }
 };
